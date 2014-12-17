dc27df09a907b593cc5fc4e4391784504960e263
salt42 <boluuuu@gmail.com>
2014-12-17 18:11:02 +0100

    -

--- bootGameEngine.js
+++ bootGameEngine.js
@@ -1,18 +1,26 @@
+define(function(require, exports, module) {
+	
     window.requestAnimFrame = (function(){
         return window.requestAnimationFrame       ||
                window.webkitRequestAnimationFrame ||
                window.mozRequestAnimationFrame    ||
                function( callback ){ window.setTimeout(callback, 1000 / 60);};
     })();
-
-    require.config({
-        baseUrl: 'src/game',
+    var baseUrl = module.uri.replace('bootGameEngine.js', 'src/game');
+	var req = requirejs.config({
+		context: 'test2',
+        baseUrl: baseUrl,
         paths: {
             jquery: '../../lib/jquery',
-        },
+        }
     });
-
-    require(['jquery', 'gameEngine'], function($, GameEngine){
+	
+    req(["require", 'jquery', 'gameEngine'], function(require, $, GameEngine){
         GameEngine.init($('#myCanvas')[0], 'PlayScene');
         GameEngine.start();
     });
+	return {
+		game:'',
+		answer: 42
+	}
+});
--- src/game/vector2.js
+++ src/game/vector2.js
@@ -1,6 +1,6 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4 */
 /*global define */
-define(function () {
+define([],function () {
 
     "use strict";
 
@@ -12,49 +12,47 @@ define(function () {
 
     Vector2.prototype.lengthSquare = function (){
             return (this.x * this.x) + (this.y * this.y);
-    }
+    };
 
 
     Vector2.prototype.length = function(){
         return Math.sqrt(this.lengthSquare()) || 0;
-    }
+    };
 
     Vector2.prototype.normalize = function(){
         this.div(this.length());
         return this;
-    }
+    };
 
     Vector2.prototype.angle = function(){
         return Math.atan2(this.y, this.x);
-    }
+    };
 
     // Start Rechenoperationen
     Vector2.prototype.add = function(vector){
         this.x += vector.x;
         this.y += vector.y;
         return this;
-    }
+    };
 
     Vector2.prototype.sub = function(vector){
         this.x -= vector.x;
         this.y -= vector.y;
         return this;
-    }
+    };
 
     Vector2.prototype.mul = function(d){
         this.x *= d;
         this.y *= d;
         return this;
-    }
+    };
 
     Vector2.prototype.div = function(d){
         if(d !== 0) this.x /= d;
         if(d !== 0) this.y /= d;
         return this;
-    }
+    };
     // Ende Rechenoperationen
 
-
     return Vector2;
-
 });
