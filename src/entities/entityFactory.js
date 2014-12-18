5491811886e343c7868db4d743530526ddddc3a8
salt42 <boluuuu@gmail.com>
2014-12-18 02:05:17 +0100

    add _parent property

--- src/entities/entityFactory.js
+++ src/entities/entityFactory.js
@@ -1,39 +1,50 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4 */
 /*global define */
-define(['entities/default/simpleShape', 'entities/default/circleShape', 'entities/default/staticShape', 'entities/default/playground'], function (SimpleShape, CircleShape, StaticShape, Playground) {
+define(['entities/default/entity', 'entities/default/simpleShape', 'entities/default/circleShape', 'entities/default/staticShape', 'entities/default/playground'], function (Entity, SimpleShape, CircleShape, StaticShape, Playground) {
 
     "use strict";
 
     var Entities = {
-            SimpleShape: SimpleShape.prototype,
-            CircleShape: CircleShape.prototype,
-            StaticShape: StaticShape.prototype,
-            Playground: Playground.prototype};
+            Entity: Entity,
+            SimpleShape: SimpleShape,
+            CircleShape: CircleShape,
+            StaticShape: StaticShape,
+            Playground: Playground };
 
     var EntityFactory = {
-
+        /*  muss nich unbedingt in die api ... man soll ja lieber createEntity benutzten
+         *  ist aber ein helper --- kanst ja noch umbauen
+         *  @param {string} name from the entity class
+         *  @return entity class pointer
+         */
+        getEntityClass: function(name) {
+            return SimpleShape;
+        },
         /*
          *  @param {string|object}+ arguments
          *  @return inherited Object with requested inheritChain
          */
         createEntity: function () {
-            var i = 0;
-            var inheritChain = [];
+            var i = 0,
+                inheritChain = [],
+                newClass;
+
             for (; i < arguments.length; i++) {
                 var currArg;
                 if(typeof arguments[i] === "string"){
-                    currArg = Entities[arguments[i]];
+                    var clas = this.getEntityClass(Entities[arguments[i]]);
+                    currArg = clas.prototype;
                 } else {
                     currArg = arguments[i];
                 }
-                if(i !== 0) {
-                    // currArg.super = inheritChain[i - 1];
+                if(i > 0) {
+                    clas.prototype._parent = inheritChain[i - 1];
                 }
                 inheritChain.push(currArg);
             }
-            console.log(inheritChain);
-            return Object.create(inheritChain[0], inheritChain[1]);
-            return Object.create.apply(null, inheritChain);
+            newClass = Object.create.apply(null, inheritChain);
+            newClass._parent = inheritChain[inheritChain.length - 1]
+            return newClass;
         },
     };
 
